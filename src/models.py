faf40d318daf8ad978c62a61553c02dfabf838d0
Frcru <91342407+Frcru@users.noreply.github.com>
2024-08-27 21:45:07 +0000

    db modeling for instagram

--- src/models.py
+++ src/models.py
@@ -1,29 +1,95 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    phone = Column(Integer, nullable=False)
+    address = Column(String(250), nullable=False)
+    created = Column(DateTime, nullable=False )
+    privacity = Column(String, nullable=False)
+    posts = relationship('Post',backref='user',lazy=True)
+    sources = relationship('Source',backref='user',lazy=True)
+    tags = relationship('Tag',backref='user',lazy=True)
+    changeusernames = relationship('Changeusername',backref='user',lazy=True)
+    followers = relationship('Follow',backref='user',lazy=True)
+    comments = relationship('Comment',backref='user',lazy=True)
+    likes = relationship('Like',backref='user',lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'        
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    is_story = Column(Boolean, nullable=False)
+    is_reel = Column(Boolean,  nullable=False)
+    is_sensitivecontent = Column(Boolean,  nullable=False)
+    is_highlighted = Column(Boolean,  nullable=False)
+    source_id = Column(Integer,ForeignKey('source.id'))
+    backgroundsong_id = Column(Integer,ForeignKey('backgroundsong.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    tags = relationship('Tag',backref='post',lazy=True)
+    comments = relationship('Comment',backref='post',lazy=True)
+    likes = relationship('Like',backref='post',lazy=True)
+
+class Source(Base):
+    __tablename__ = 'source'        
+    id = Column(Integer, primary_key=True)    
+    user_id = Column(Integer, ForeignKey('user.id'))
+    url = Column(String(250),nullable=False)
+    size = Column(String(250),nullable=False)
+    duration = Column(Integer,nullable=False)
+    posts = relationship('Post',backref='source',lazy=True)
+
+class Backgroundsong(Base):
+    __tablename__ = 'backgroundsong'        
+    id = Column(Integer, primary_key=True)    
+    name = Column(String(80),nullable=False)
+    duration = Column(Integer,nullable=False)
+    author = Column(String(80),nullable=False)
+    band = Column(String(80),nullable=False)
+    posts = relationship('Post',backref='backgroundsong',lazy=True)
+    
+class Tag(Base):
+    __tablename__ = 'tag'        
+    id = Column(Integer, primary_key=True)    
+    user_id = Column(Integer,ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class ChangeUsername(Base):
+    __tablename__ = 'changeusername'
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    oldname = Column(Integer,nullable=False)
+    newname = Column(Integer,nullable=False)
+
+class Follow(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer,primary_key=True)
+    fromuser_id = Column(Integer,ForeignKey('user.id'))
+    touser_id = Column(Integer,ForeignKey('user.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer,primary_key=True)
+    commentary = Column(String(250),primary_key=True)
+    timestamp = Column(DateTime, nullable=False)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    post_id = Column(Integer,ForeignKey('post.id'))
+    likes = relationship('Like',backref='comment',lazy=True)
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    post_id = Column(Integer,ForeignKey('post.id'))
+    comment_id = Column(Integer,ForeignKey('comment.id'))
+
 
     def to_dict(self):
         return {}
